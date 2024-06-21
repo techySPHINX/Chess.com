55ea4cdc443101f3d615f7bf622558272111cb79
techySPHINX <shimon123universe@gmail.com>
2024-06-22 01:55:12 +0530

    board rendered for client side

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,77 @@
-const socket = io();
+const socket = io(); //initiated
 
-socket.emit("")
+const chess = new Chess();
+
+const boardElement = document.querySelector(".chessboard");
+
+//implement drag and drop functionality
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+//functions for client side
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+
+  //for checked pattern
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = squareindex;
+
+      // if element is present or not means
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", () => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowindex, col: squareindex };
+            e.dataTransfer.setData("text/plain", ""); // for cross-platform
+          }
+        });
+
+        pieceElement.addEventListener("dragend", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+
+        squareElement.appendChild(pieceElement);
+      }
+
+      squareElement.addEventListener("dragover", function (e) {
+        e.preventDefault();
+      });
+
+      squareElement.addEventListener("drop", function (e) {
+        e.preventDefault();
+        if (draggedPiece) {
+          const targetSource = {
+            row: squareElement.dataset.row,
+            col: squareElement.dataset.col,
+          };
+        }
+      });
+    });
+  });
+};
+const handleMove = () => {};
+const getPieceUnicode = () => {};
+
+renderBoard();
